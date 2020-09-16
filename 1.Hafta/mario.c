a0d9d4f5b1b245c801345578fad089cc29294f63
Onur Ferik <49190459+nyf16@users.noreply.github.com>
2020-09-16 16:38:33 +0300

    Create mario.c

---
+++ 1.Hafta/mario.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void space(int k)
+{
+    for (int i=0; i<k; i++) 
+    printf(" ");
+}
+void stars(int k)
+{
+    for (int i=0; i<k; i++) 
+    printf("#");
+    printf("\n");
+}
+
+void square(int k)
+{
+    while (k < 1 || k > 8)
+        k = get_int("Lütfen 1-8 arasında yüksekliği belirtiniz: ");
+    int star = 1;
+    for (int i=1; i<=k; i++ , star++){
+        space(k-i);
+        stars(star);
+    }
+}
+
+int main(void)
+{
+    int k = get_int("Lütfen 1-8 arasında yüksekliği belirtiniz: ");
+    square(k);
+}
+
