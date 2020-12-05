30d118023a614d03b47a56087fca4ce314980151
Onur Ferik <49190459+nyf16@users.noreply.github.com>
2020-12-05 21:09:33 +0300

    Update

--- 2.Hafta/caesar.c
+++ 2.Hafta/caesar.c
@@ -18,7 +18,7 @@ bool number(char text[],const int size)
 int convertNumber(char number[],const int size)
 {
     int num = 0;
-    for (int i = 0; i<size ; i++)
+    for (int i = 0; i < size ; i++)
     {
         num *= 10;
         num += number[i] - '0'; 
